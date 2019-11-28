3b280eb7df35d7ca4b15b776264e4d80e7c6331f
Ikaru90 <zabio90@gmail.com>
2019-11-28 17:56:30 +0300

    temporary removed oauth

--- src/main/java/com/forumservice/repo/UserDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.forumservice.repo;
-
-import com.forumservice.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsRepo extends JpaRepository<User, String> {
-}
