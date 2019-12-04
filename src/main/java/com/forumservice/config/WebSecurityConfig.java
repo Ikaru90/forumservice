f61f23271a2e5107d9d172bcb2bfaa09116d86b7
Ikaru90 <zabio90@gmail.com>
2019-12-04 18:58:52 +0300

    spring security

---
+++ src/main/java/com/forumservice/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.forumservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().cors()
+                .and().csrf().disable();
+    }
+}
